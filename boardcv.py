96303722ec28d71089541ad01e1eddb27c56fbcc
amfl <amfl@none.none>
2021-02-14 13:41:38 +1300

    refactor

--- boardcv.py
+++ boardcv.py
@@ -10,28 +10,19 @@ class BoardTracker:
         self.dimensions = (board_y, board_x)  # numpy order
         self.padding = border_padding
 
-    def draw_piece_debug(self, frame):
-        border_px = 26 # TODO: Make proportion to be res independent
-        inner = (frame.shape[0] - border_px,
-                 frame.shape[1] - border_px)
-
-        frame_blur = cv2.blur(frame, (9,9))
-
-        for y in range(self.dimensions[1]):
-            for x in range(self.dimensions[0]):
-                py = int(y * (inner[0] / self.dimensions[0])) + border_px
-                px = int(x * (inner[1] / self.dimensions[1])) + border_px
-                # Not sure why I need to convert these to int
-                color = frame_blur[py,px]
-                color = (int(color[0]), int(color[1]), int(color[2]))
-                cv2.rectangle(frame, (px-10,py-10),(px+10,py+10),color,-1)
-        return frame
-
     def update(self, frame):
         frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         board_mask = generate_board_mask(frame_HSV)
         frame_mask_blur = cv2.blur(board_mask, (3,3))
 
+        self._update_board_corners(frame_mask_blur)
+
+        frame_birds_eye = BoardTracker.get_frame_birds_eye(frame, self.get_corner_estimate())
+        frame_debug = self._update_board_state_estimate(frame_birds_eye)
+
+        return frame_debug
+
+    def _update_board_corners(self, frame_mask_blur):
         # Detect edges using Canny
         threshold = 250
         canny_output = cv2.Canny(frame_mask_blur, threshold, threshold * 3)
@@ -48,8 +39,41 @@ class BoardTracker:
         if len(contour_hull) == 4:
             self.corners = contour_hull
 
+    def _update_board_state_estimate(self, frame_birds_eye):
+        border_px = 26 # TODO: Make proportion to be res independent
+        inner = (frame_birds_eye.shape[0] - border_px,
+                 frame_birds_eye.shape[1] - border_px)
+
+        frame_blur = cv2.blur(frame_birds_eye, (9,9))
+
+        for y in range(self.dimensions[1]):
+            for x in range(self.dimensions[0]):
+                py = int(y * (inner[0] / self.dimensions[0])) + border_px
+                px = int(x * (inner[1] / self.dimensions[1])) + border_px
+                # Not sure why I need to convert these to int
+                color = frame_blur[py,px]
+                color = (int(color[0]), int(color[1]), int(color[2]))
+                cv2.rectangle(frame_birds_eye, (px-10,py-10),(px+10,py+10),color,-1)
+
+        # Returns frame with debug info
+        return frame_birds_eye
+
+    @staticmethod
+    def get_frame_birds_eye(frame, corner_estimate):
+        """Uses the current corner estimate to transform the board to top-down"""
+        out_w, out_h = 640, 640
+        src = np.float32(corner_estimate)
+        dest = np.float32([[out_w, 0], [out_w, out_h], [0, out_h], [0, 0]])
+        # Apply Perspective Transform Algorithm
+        matrix = cv2.getPerspectiveTransform(src, dest)
+        result = cv2.warpPerspective(frame, matrix, (out_w, out_h))
+        return result
+
     def get_corner_estimate(self):
         return self.corners
+
+    def get_board_state_estimate(self):
+        pass
 
 def generate_board_mask(frame_hsv):
     """Isolates the rough shape of the board by color matching.
--- main.py
+++ main.py
@@ -9,46 +9,39 @@ def main():
     video_source = sys.argv[1]
     video_cap = vidio.get_video_cap(video_source)
 
-    frame_num = 0
-    board_mask = None
+    num_frames = 0  # frame counter
+    frame_debug = None
     tracker = boardcv.BoardTracker()
 
     while True:
         ret, frame = video_cap.read()
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        if frame_num % 10 == 0:  # TODO: Do this per time rather than frames
+        # # TEMP: Draw straight onto the image before giving it to the tracker
+        # corners = tracker.get_corner_estimate()
+        # cv2.drawContours(frame, [corners], -1, (255,255,255), 2)
+
+        # # Slam a marker at the board corners
+        # for corner in corners:
+        #     x,y= corner[0]
+        #     x= int(x)
+        #     y= int(y)
+        #     cv2.rectangle(frame, (x-10,y-10),(x+10,y+10),(0,0,255),-1)
+
+        if num_frames % 10 == 0:  # TODO: Do this per time rather than frames
             # Detect the board
-            tracker.update(frame)
+            frame_debug = tracker.update(frame)
 
-        # Draw onto the image
 
-        corners = tracker.get_corner_estimate()
-        cv2.drawContours(frame, [corners], -1, (255,255,255), 2)
 
-        # Slam a marker at the board corners
-        for corner in corners:
-            x,y= corner[0]
-            x= int(x)
-            y= int(y)
-            cv2.rectangle(frame, (x-10,y-10),(x+10,y+10),(0,0,255),-1)
+        # result = tracker.draw_piece_debug(result)
 
-        # Obtain a birds eye view
-        out_w, out_h = 640, 640
-        src = np.float32(tracker.get_corner_estimate())
-        dest = np.float32([[out_w, 0], [out_w, out_h], [0, out_h], [0, 0]])
-        # Apply Perspective Transform Algorithm
-        matrix = cv2.getPerspectiveTransform(src, dest)
-        result = cv2.warpPerspective(frame, matrix, (out_w, out_h))
-
-        result = tracker.draw_piece_debug(result)
-
-        cv2.imshow("Program Output", result)
+        cv2.imshow("Program Output", frame_debug)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-        frame_num += 1
+        num_frames += 1
 
     # After the loop release the cap object
     video_cap.release()
